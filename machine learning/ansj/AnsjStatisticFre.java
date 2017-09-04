532e41e35a4c438c73ccbbc3e530bb0ec8ca26e5
xujie <jiexu@pptv.com>
2017-09-04 14:55:07 +0800

    优化ansj分词

--- machine learning/ansj/AnsjStatisticFre.java
+++ machine learning/ansj/AnsjStatisticFre.java
@@ -9,13 +9,15 @@ public class AnsjStatisticFre {
     private List<File> fileList = new ArrayList<File>();
     private Set<String> expectedNature = new HashSet<String>();
     private Map<String, Integer> word_freq = new HashMap<String, Integer>();
+    private String result_file = new String();
 
     public int GetFileSize() {
       return this.fileList.size();
     }
 
-    public int Init(String filePath) {
+    public int Init(String filePath, String res_file) {
       //遍历文件夹
+      this.result_file = res_file;
       File dir = new File(filePath);
       File[] files = dir.listFiles(); // 该文件目录下文件全部放入数组
       if (files == null) {
@@ -26,7 +28,7 @@ public class AnsjStatisticFre {
       for (int i = 0; i < files.length; i++) {
         String fileName = files[i].getName();
         if (files[i].isDirectory()) { // 判断是文件还是文件夹
-          this.Init(files[i].getAbsolutePath()); // 获取文件绝对路径
+          this.Init(files[i].getAbsolutePath(), this.result_file); // 获取文件绝对路径
         } else { // 判断是否是文件
           if (files[i] != null) {
             fileList.add(files[i]);
@@ -56,39 +58,55 @@ public class AnsjStatisticFre {
     }
 
     //分词
-    public void start(String res_file) {
+    public void start() {
         for (int i = 0; i < this.fileList.size(); i++) {
           BufferedReader reader = null;
+          FileReader fr = null;
           try {
-            reader = new BufferedReader(new FileReader(new File(this.fileList.get(i).getAbsolutePath())));
-            String temp_str = null;
-            String total_string = null;
+            fr = new FileReader(this.fileList.get(i).getAbsolutePath());
+            reader = new BufferedReader(fr);
+
+            System.out.println("start file " + this.fileList.get(i).getAbsolutePath());
+            String temp_str = new String();
             while ((temp_str = reader.readLine()) != null) {
-              total_string += temp_str;
-            }
-            Result result = ToAnalysis.parse(total_string);
-            List<Term> terms = result.getTerms();
-            for(int j=0; j<terms.size(); j++) {
-              String natureStr = terms.get(j).getNatureStr();
-              if(this.expectedNature.contains(natureStr)) {
-                if (this.word_freq.containsKey(terms.get(j).getName()) != false) {
-                  int count = this.word_freq.get(terms.get(j).getName()) + 1;
-                  this.word_freq.put(terms.get(j).getName(), count);
-                }
-                else {
-                  this.word_freq.put(terms.get(j).getName(), 1);
+              List<Term> terms = ToAnalysis.parse(temp_str).getTerms();
+              for(int j=0; j<terms.size(); j++) {
+                if(this.expectedNature.contains(terms.get(j).getNatureStr())) {
+                  if (this.word_freq.containsKey(terms.get(j).getName()) != false) {
+                    int count = this.word_freq.get(terms.get(j).getName()) + 1;
+                    this.word_freq.put(terms.get(j).getName(), count);
+                  }
+                  else {
+                    this.word_freq.put(terms.get(j).getName(), 1);
+                  }
                 }
               }
             }
-          } catch (IOException e) {
-            System.out.println(e.getMessage());
+
+            if (fr != null && reader != null) {
+              fr.close();
+              reader.close();
+            }
+
+            System.out.println("word freq size " + this.word_freq.size());
+          } catch (Exception e) {
+            System.out.println("first " + e.getMessage());
+            if (fr != null && reader != null) {
+              try {
+                fr.close();
+                reader.close();
+              }
+              catch (Exception ioe) {
+                System.out.println("second " + ioe.getMessage());
+              }
+            }
             continue;
           }
         }
 
         try {
           System.out.println("begin write result");
-          FileWriter fw = new FileWriter(res_file);
+          FileWriter fw = new FileWriter(this.result_file);
           BufferedWriter bw = new BufferedWriter(fw);
           for (Map.Entry<String, Integer> entry : this.word_freq.entrySet()) {
             String key = entry.getKey().toString();
@@ -100,9 +118,8 @@ public class AnsjStatisticFre {
           fw.close();
           bw.close();
           System.out.println("write result end");
-        } catch (IOException e) {
-          System.out.println("write result err");
-          System.out.println(e.getMessage());
+        } catch (Exception e) {
+          System.out.println("write result err " + e.getMessage());
         }
       }
 
@@ -110,13 +127,13 @@ public class AnsjStatisticFre {
         AnsjStatisticFre fa = new AnsjStatisticFre();
         String file_path = "./";
         String conf_file = "";
-        String result_file = "";
-        if (fa.Init(file_path) < 1) {
+        String result_file = "result.data";
+        if (fa.Init(file_path, result_file) < 1) {
           System.out.println("init error " + file_path);
         }
         else {
-          System.out.printf("file list len %d\n", fa.GetFileSize());
+          System.out.println("file list len " + fa.GetFileSize());
         }
-        fa.start(result_file);
+        fa.start();
     }
 }
