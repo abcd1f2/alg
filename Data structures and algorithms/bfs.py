a50407c4c4c325b2b9018cb98f385ad2ecd0673c
xujie <jiexu@pptv.com>
2017-09-15 16:16:51 +0800

    add graph and update bfs

--- Data structures and algorithms/bfs.py
+++ Data structures and algorithms/bfs.py
@@ -2,28 +2,25 @@
 
 '''
 @parameter
-    ll: arrary
     st: start search
 '''
-def bfs(ll, st):
+def bfs(st):
     tmp = [st,]
-    flags = [0 for i in xrange(len(ll) + 1)]
+    flags = [0 for i in xrange(len(double_list) + 1)]
     flags[st] = 1
     while len(tmp) > 0:
         i = tmp[0]
         tmp.pop(0)
         print i
-        for j in xrange(1, len(ll) + 1):
-            if flags[j] <= 0 and ll[i-1][j-1] > 0:
+        for j in xrange(1, len(double_list) + 1):
+            if flags[j] <= 0 and double_list[i-1][j-1] > 0:
                 tmp.append(j)
                 flags[j] = 1
 
 if __name__ == '__main__':
     double_list = [[0, 1, 1, 0, 0],
-    [0, 0, 1, 1, 0],
-    [0, 1, 1, 1, 0],
-    [1, 0, 0, 0, 0],
-    [0, 0, 1, 1, 0]]
-    for i in xrange(1, len(double_list) + 1):
-        print "i ", i
-        bfs(double_list, i)
+            [0, 0, 1, 1, 0],
+            [0, 1, 1, 1, 0],
+            [1, 0, 0, 0, 0],
+            [0, 0, 1, 1, 0]]
+    bfs(0)
