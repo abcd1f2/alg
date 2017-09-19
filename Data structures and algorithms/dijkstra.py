9ac926b604e5dc10274c10b267b3adb7ba48e9f3
xujie <jiexu@pptv.com>
2017-09-19 11:05:26 +0800

    update dijkastra

--- Data structures and algorithms/dijkstra.py
+++ Data structures and algorithms/dijkstra.py
@@ -21,9 +21,11 @@ def dijkstra(doub_list, vertexs, edges):
 
     #计算所有中间顶点
     for i in xrange(1, vertexs):
-        v_min = min_value
-
-        #将未纳入的顶点纳入到计算集合中
+        v_min = x
+        """
+        将未纳入的顶点纳入到计算集合中
+        1.选出距离开始点最近的一个松弛
+        """
         for j in xrange(1, vertexs + 1):
             if book[j] == 0 and dist[j] < v_min:
                 v_min = dist[j]
@@ -32,21 +34,21 @@ def dijkstra(doub_list, vertexs, edges):
 
         '''
         优化点：将边的权重加入到一个堆中，然后从堆中取最小的值
+        将刚纳入的顶点进行计算，围绕顶点计算周围的最近顶点
+        2.将选出的松弛进行最终距离的确定
         '''
-        #将刚纳入的顶点进行计算，围绕顶点计算周围的最近顶点
         for v in xrange(1, vertexs + 1):
-            if doub_list[u][v] < min_value and dist[v] > dist[u] + doub_list[u][v]:
+            if doub_list[u][v] < x and dist[v] > dist[u] + doub_list[u][v]:
                 dist[v] = dist[u] + doub_list[u][v]
     return dist
 
 if __name__ == "__main__":
-    min_value = 999999999
-    ll = [
-    [min_value,min_value,min_value,min_value,min_value,min_value,min_value],
-    [min_value,0,1,12,min_value, min_value, min_value],
-    [min_value,min_value,0,9,3,min_value,min_value],
-    [min_value,min_value,min_value,0,min_value,5,min_value],
-    [min_value,min_value,min_value,4,0,13,15],
-    [min_value,min_value,min_value,min_value,min_value,0,4],
-    [min_value,min_value,min_value,min_value,min_value,min_value,0]]
+    x = 999999999
+    ll = [[x,x,x,x,x,x,x],
+        [x,0,1,12,x, x, x],
+        [x,x,0,9,3,x,x],
+        [x,x,x,0,x,5,x],
+        [x,x,x,4,0,13,15],
+        [x,x,x,x,x,0,4],
+        [x,x,x,x,x,x,0]]
     print dijkstra(ll, 6, 9)
